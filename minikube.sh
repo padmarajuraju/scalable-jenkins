64b13125ac66d00d2b6ba28146bf60bd1730a616
padmarajuraju <111412403+padmarajuraju@users.noreply.github.com>
2023-02-14 18:06:16 +0530

    Add files via upload

---
+++ minikube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+sudo apt-get update -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+minikube config set cpus 2
+minikube config set memory 3999
+
+minikube start
+
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
